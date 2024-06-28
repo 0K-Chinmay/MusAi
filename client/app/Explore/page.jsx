70f7ba2a2c63a2b0223ef67dd7a89263ed7d2e5e
0K-Chinmay <133075305+0K-Chinmay@users.noreply.github.com>
2024-06-28 15:51:21 +0530

    Update page.jsx

--- client/app/Explore/page.jsx
+++ client/app/Explore/page.jsx
@@ -29,7 +29,7 @@ setinfo(await response.json())
     <Nav/>
     <div className='absolute h-full w-[100%] z-0 bottom-0 flex
      flex-wrap justify-center items-center' >
-      <RxCross2 className={pl?' absolute z-10 cursor-pointer top-14 right-8':'hidden'} size={40} onClick={()=>{setpl(false)}} />
+      <RxCross2 className={pl?'fixed z-10 cursor-pointer top-[6rem] right-2':'hidden'} size={40} onClick={()=>{setpl(false)}} />
       <div className=' h-[80%] w-[88%]  flex items-center justify-center gap-[5rem] flex-wrap'>
       {info.map((n,i)=>
       <div className='m-0 cursor-pointer'onClick={()=>{setpl(true);setlinks(i)}}>
@@ -38,7 +38,7 @@ setinfo(await response.json())
       )}
       </div>
       {pl?
-      <div className='absolute bg-black h-screen w-screen flex items-center justify-center bg-opacity-[0.6]'>
+      <div className='fixed bg-black h-screen w-screen flex items-center justify-center bg-opacity-[0.6] top-[3rem]'>
       <Player  includeTags={false} includeSearch={false} trackList={info[links]?.songInfo}/>
       </div>
       :<></>}
