2a1667aa93f772b2f3fac4500266355998ce9e6d
0K-Chinmay <133075305+0K-Chinmay@users.noreply.github.com>
2024-06-29 20:07:14 +0530

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const User =new mongoose.Schema({
     secret:{type:String , required:true },
     playlist:{type:Array , required:true},
-    prompt:{type:String , required:true, unique:true},
+    prompt:{type:String , required:true},
     songInfo:{type:Array,maxItems:7 },
     quote :{type:String},
 }, {collection:'user'})
