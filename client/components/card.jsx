076e5cdcf11f4118a01d3625dbeab9569f48a7b5
0K-Chinmay <133075305+0K-Chinmay@users.noreply.github.com>
2024-06-29 20:12:18 +0530

    Update card.jsx

--- client/components/card.jsx
+++ client/components/card.jsx
@@ -11,7 +11,7 @@ function card({iname}) {
     <img className="h-[20rem] w-full object-cover rounded-md" src="https://images.unsplash.com/photo-1680725779155-456faadefa26" alt="Random image"/>
     <div className="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
     <div className="absolute inset-0 flex items-center justify-center">
-        <h2 className="text-white text-3xl font-bold">{iname.prompt}</h2>
+        <h2 className="text-white text-3xl font-bold capitalize">{iname.prompt}</h2>
     </div>
 </div>
   )
